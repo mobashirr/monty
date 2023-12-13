ca0dcb6d1d224560939c11d1e7a246ecf42bc685
sozoia <zfzg2232@gmail.com>
2023-12-13 22:07:42 +0300

    fix sub op

--- arthmatic.c
+++ arthmatic.c
@@ -23,22 +23,29 @@ int add(void)
 }
 
 /**
- * add - subtract top two elments of the stack x(n) - x(n-1)
+ * add - subtract top two elments of the stack x(n-1) - x(n)
+ * where n is the order of last pushed element 
  * Return: 0 if success else 1
 */
 int sub(void)
 {
-	int i;
+    int i;
 
-	if (linked && linked->next)
-	{
-		i = linked->next->n - linked->n;
-		linked = linked->next;
-		linked->n = i;
-		free(linked->prev);
-		linked->prev = NULL;
-		return (0);
-	}
-	else
-		return (1);
+    if (linked && linked->next)
+    {
+        i = linked->n;
+        linked = linked->next;
+
+        linked->n = linked->n - i;
+
+        linked->prev->next = NULL;
+        free(linked->prev);
+
+        linked->prev = NULL;
+
+        return 0;
+    } 
+    else
+        return 1;
+
 }
